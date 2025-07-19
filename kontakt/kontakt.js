95ccec1bbaa852dddc68670c48f04a450bca3fd3
xXGrowGuruXx <116651235+xXGrowGuruXx@users.noreply.github.com>
2025-07-20 00:14:14 +0200

    Update Style 3.0

--- kontakt/kontakt.js
+++ kontakt/kontakt.js
@@ -1,15 +1,30 @@
-function SetKalender(){
+function SetKalender() {
     const today = new Date();
     const in3Weeks = new Date();
     in3Weeks.setDate(today.getDate() + 21);
 
     flatpickr("#datum", {
-      minDate: today,
-      maxDate: in3Weeks,
-      dateFormat: "Y-m-d",
-      disableMobile: false
+        minDate: today,
+        maxDate: in3Weeks,
+        dateFormat: "Y-m-d",
+        disableMobile: false
     });
 }
+
+function validateEmail(mail) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,6}$/;
+    return emailRegex.test(mail);
+}
+
+const emailInput = document.getElementById('email');
+emailInput.addEventListener('blur', () => {
+    if (!validateEmail(emailInput.value)) {
+        emailInput.setCustomValidity('Bitte gib eine gültige E-Mail-Adresse ein.');
+    } else {
+        emailInput.setCustomValidity('');
+    }
+    emailInput.reportValidity();
+});
 
 document.addEventListener('DOMContentLoaded', () => {
     const select = document.getElementById('anfrageArt');
